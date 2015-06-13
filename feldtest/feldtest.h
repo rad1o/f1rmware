73a3f6aa38ff8051304b0869d135fa4474fe5d67
Stefan `Sec` Zehl <sec@42.org>
2015-06-13 21:19:14 +0200

    feldtest V5

--- feldtest/feldtest.h
+++ feldtest/feldtest.h
@@ -12,16 +12,6 @@
 #define BY_AMP_N_GPORT                GPIO5
 #define BY_AMP_N_GPIN                 GPIOPIN5
  
-//enter
-#define TX_AMP_PIN                  (P5_6)
-#define TX_AMP_FUNC                 (SCU_CONF_FUNCTION0)
-#define TX_AMP_GPORT                GPIO2
-#define TX_AMP_GPIN                 GPIOPIN15
-#define RX_LNA_PIN                  (P6_7)
-#define RX_LNA_FUNC                 (SCU_CONF_FUNCTION4)
-#define RX_LNA_GPORT                GPIO5
-#define RX_LNA_GPIN                 GPIOPIN15
- 
 //left
 #define TX_RX_PIN                   (P2_10)
 #define TX_RX_FUNC                 (SCU_CONF_FUNCTION0)
@@ -47,6 +37,23 @@
 #define LOW_HIGH_FILT_N_FUNC                 (SCU_CONF_FUNCTION0)
 #define LOW_HIGH_FILT_N_GPORT                GPIO2
 #define LOW_HIGH_FILT_N_GPIN                 GPIOPIN12
+
+//down2
+#define TX_AMP_PIN                  (P5_6)
+#define TX_AMP_FUNC                 (SCU_CONF_FUNCTION0)
+#define TX_AMP_GPORT                GPIO2
+#define TX_AMP_GPIN                 GPIOPIN15
+#define RX_LNA_PIN                  (P6_7)
+#define RX_LNA_FUNC                 (SCU_CONF_FUNCTION4)
+#define RX_LNA_GPORT                GPIO5
+#define RX_LNA_GPIN                 GPIOPIN15
+ 
+//left2
+#define CS_VCO_PIN   (P5_4)
+#define CS_VCO_FUNC  (SCU_CONF_FUNCTION0)
+#define CS_VCO_GPORT GPIO2
+#define CS_VCO_GPIN  GPIOPIN13
+
 
 
 void feldInit();
--- feldtest/main.c
+++ feldtest/main.c
@@ -59,72 +59,118 @@ int main(void)
     lcdInit();
     lcdFill(0xff);
 	setSystemFont();
-	char tu=0,td=0,tl=0,tr=0,tm=0;
-	lcdPrintln("Feld-Test v2");
-	lcdPrintln("");
+	char tu=0,td=0,tl=0,tr=0;
+	char page=0;
+	char tu2=0,td2=0,tl2=0,tr2=0;
 
 	int ctr=0;
 	int k=0;
 	/* Blink LED1 on the board. */
 	while (1) 
 	{
-		lcdSetCrsr(0,16);
-		lcdPrint(IntToStr(tu,2,F_HEX)); lcdPrintln(" Up   BY_MIX");
-		lcdPrint(IntToStr(td,2,F_HEX)); lcdPrintln(" Down BY_AMP");
-		lcdPrint(IntToStr(tl,2,F_HEX)); lcdPrintln(" Left TX_RX");
-		lcdPrint(IntToStr(tr,2,F_HEX)); lcdPrintln(" Right LOW_HIGH_FILT");
-		lcdPrint(IntToStr(tm,2,F_HEX)); lcdPrintln(" Enter TX_AMP|RX_LNA");
-		lcdDisplay(); 
-		switch(getInput()){
-			case BTN_UP:
-				tu=1-tu;
-				if (tu){
-					gpio_set(BY_MIX_GPORT,BY_MIX_GPIN);
-				}else{
-					gpio_clear(BY_MIX_GPORT,BY_MIX_GPIN);
-				};
-				break;
-			case BTN_DOWN:
-				td=1-td;
-				if (td){
-					gpio_set(BY_AMP_GPORT,BY_AMP_GPIN);
-					gpio_clear(BY_AMP_N_GPORT,BY_AMP_N_GPIN);
-				}else{
-					gpio_clear(BY_AMP_GPORT,BY_AMP_GPIN);
-					gpio_set(BY_AMP_N_GPORT,BY_AMP_N_GPIN);
-				};
-				break;
-			case BTN_LEFT:
-				tl=1-tl;
-				if (tl){
-					gpio_set(TX_RX_GPORT,TX_RX_GPIN);
-					gpio_clear(TX_RX_N_GPORT,TX_RX_N_GPIN);
-				}else{
-					gpio_clear(TX_RX_GPORT,TX_RX_GPIN);
-					gpio_set(TX_RX_N_GPORT,TX_RX_N_GPIN);
-				};
-				break;
-			case BTN_RIGHT:
-				tr=1-tr;
-				if (tr){
-					gpio_set(LOW_HIGH_FILT_GPORT,LOW_HIGH_FILT_GPIN);
-					gpio_clear(LOW_HIGH_FILT_N_GPORT,LOW_HIGH_FILT_N_GPIN);
-				}else{
-					gpio_clear(LOW_HIGH_FILT_GPORT,LOW_HIGH_FILT_GPIN);
-					gpio_set(LOW_HIGH_FILT_N_GPORT,LOW_HIGH_FILT_N_GPIN);
-				};
-				break;
-			case BTN_ENTER:
-				tm=1-tm;
-				if (tm){
-					gpio_set(TX_AMP_GPORT,TX_AMP_GPIN);
-					gpio_set(RX_LNA_GPORT,RX_LNA_GPIN);
-				}else{
-					gpio_clear(TX_AMP_GPORT,TX_AMP_GPIN);
-					gpio_clear(RX_LNA_GPORT,RX_LNA_GPIN);
-				};
-				break;
+		lcdClear(0xff);
+		lcdPrintln("Feld-Test v5");
+		lcdPrintln("");
+		lcdPrint("Page ");lcdPrintln(IntToStr(page,2,0));
+		if (page==0){
+			lcdPrint(IntToStr(tu,2,F_HEX)); lcdPrintln(" Up   BY_MIX");
+			lcdPrint(IntToStr(td,2,F_HEX)); lcdPrintln(" Down BY_AMP");
+			lcdPrint(IntToStr(tl,2,F_HEX)); lcdPrintln(" Left TX_RX");
+			lcdPrint(IntToStr(tr,2,F_HEX)); lcdPrintln(" Right LOW_HIGH_FILT");
+			lcdPrintln("Enter for next page");
+			lcdDisplay(); 
+			switch(getInput()){
+				case BTN_UP:
+					tu=1-tu;
+					if (tu){
+						gpio_set(BY_MIX_GPORT,BY_MIX_GPIN);
+					}else{
+						gpio_clear(BY_MIX_GPORT,BY_MIX_GPIN);
+					};
+					break;
+				case BTN_DOWN:
+					td=1-td;
+					if (td){
+						gpio_set(BY_AMP_GPORT,BY_AMP_GPIN);
+						gpio_clear(BY_AMP_N_GPORT,BY_AMP_N_GPIN);
+					}else{
+						gpio_clear(BY_AMP_GPORT,BY_AMP_GPIN);
+						gpio_set(BY_AMP_N_GPORT,BY_AMP_N_GPIN);
+					};
+					break;
+				case BTN_LEFT:
+					tl=1-tl;
+					if (tl){
+						gpio_set(TX_RX_GPORT,TX_RX_GPIN);
+						gpio_clear(TX_RX_N_GPORT,TX_RX_N_GPIN);
+					}else{
+						gpio_clear(TX_RX_GPORT,TX_RX_GPIN);
+						gpio_set(TX_RX_N_GPORT,TX_RX_N_GPIN);
+					};
+					break;
+				case BTN_RIGHT:
+					tr=1-tr;
+					if (tr){
+						gpio_set(LOW_HIGH_FILT_GPORT,LOW_HIGH_FILT_GPIN);
+						gpio_clear(LOW_HIGH_FILT_N_GPORT,LOW_HIGH_FILT_N_GPIN);
+					}else{
+						gpio_clear(LOW_HIGH_FILT_GPORT,LOW_HIGH_FILT_GPIN);
+						gpio_set(LOW_HIGH_FILT_N_GPORT,LOW_HIGH_FILT_N_GPIN);
+					};
+					break;
+				case BTN_ENTER:
+					page++;
+					break;
+			};
+		}else if (page==1){
+			lcdPrint(IntToStr(tu2,2,F_HEX)); lcdPrintln(" Up   ");
+			lcdPrint(IntToStr(td2,2,F_HEX)); lcdPrintln(" Down TX_AMP|~RX_LNA");
+			lcdPrint(IntToStr(tl2,2,F_HEX)); lcdPrintln(" Left CS_VCO");
+			lcdPrint(IntToStr(tr2,2,F_HEX)); lcdPrintln(" Right ");
+			lcdPrintln("Enter for next page");
+			lcdDisplay(); 
+			switch(getInput()){
+				case BTN_UP:
+					tu2=1-tu2;
+					if (tu2){
+						;
+					}else{
+						;
+					};
+					break;
+				case BTN_DOWN:
+					td2=1-td2;
+					if (td2){
+						gpio_set(TX_AMP_GPORT,TX_AMP_GPIN);
+						gpio_clear(RX_LNA_GPORT,RX_LNA_GPIN);
+					}else{
+						gpio_clear(TX_AMP_GPORT,TX_AMP_GPIN);
+						gpio_set(RX_LNA_GPORT,RX_LNA_GPIN);
+					};
+					break;
+				case BTN_LEFT:
+					tl2=1-tl2;
+					if (tl2){
+						gpio_set(CS_VCO_GPORT,CS_VCO_GPIN);
+					}else{
+						gpio_clear(CS_VCO_GPORT,CS_VCO_GPIN);
+					};
+					break;
+				case BTN_RIGHT:
+					tr2=1-tr2;
+					if (tr2){
+						;
+					}else{
+						;
+					};
+					break;
+				case BTN_ENTER:
+					page++;
+					break;
+			};
+
 		};
+		if (page>1){page=0;}
 
 		gpio_set(LED1_GPORT, LED1_GPIN); /* LED off */
 		delay(200000);
