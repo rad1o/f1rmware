4eac114a1b38e41ec01be48325cce2b4dd01f07a
Stefan `Sec` Zehl <sec@42.org>
2015-07-02 12:21:33 +0200

    Clean up bootloader code

--- bootloader/main.c
+++ bootloader/main.c
@@ -48,6 +48,7 @@
 #include "../fatfs/ff.h"
 #include <pins.h>
 #include <keyin.h>
+#include "intrinsics.h"
 
 static uint16_t counter=0;
 
@@ -70,55 +71,21 @@ void doMSC(){
 	dwim();
 };
 
-static inline uint32_t get_sp(void){
-	register uint32_t result;
-	__asm volatile ("MRS %0, msp\n" : "=r" (result) );
-	return result;
-};
+extern void * _text_end;
+extern void * _end;
 
-
-static inline void boot(const void * vtable){
-	// Set new Vtable
-	SCB_VTOR = (uintptr_t) vtable;  
-
-	// Reset stack pointer & branch to the new reset vector.  
-	__asm(  "mov r0, %0\n"  
-			"ldr sp, [r0]\n"  
-			"ldr r0, [r0, #4]\n"  
-			"bx r0\n"  
-			:  
-			: "r"(vtable)
-			: "%sp", "r0");  
-};
-
-static inline uint32_t get_pc(void){
-	register uint32_t result;
-	__asm volatile ("mov %0, pc\n" : "=r" (result) );
-	return result;
-};
-
-extern void * end;
-extern uint32_t * _text_start;
 volatile uint32_t global=42;
 
-extern unsigned _data_loadaddr, _data, _edata, _bss, _ebss, _stack;
-
-
-void doCopy(){
+void doInfo(){
 	lcdClear(0xff);
 	lcdNl();
-	lcdPrint("PC: "); lcdPrint(IntToStr(get_pc(),8,F_HEX));lcdNl();
-	lcdPrint("SP: "); lcdPrint(IntToStr(get_sp(),8,F_HEX));lcdNl();
-	lcdPrint("SR: "); lcdPrint(IntToStr(CREG_M4MEMMAP,8,F_HEX));lcdNl();
-/*	lcdPrint("end: "); lcdPrint(IntToStr((uintptr_t)&end,8,F_HEX));lcdNl();
-	lcdPrint("ts: "); lcdPrint(IntToStr((uintptr_t)&_text_start,8,F_HEX));lcdNl();
-	lcdPrint("dl: "); lcdPrint(IntToStr((uintptr_t)&_data_loadaddr,8,F_HEX));lcdNl();
-	lcdPrint("d: "); lcdPrint(IntToStr((uintptr_t)&_data,8,F_HEX));lcdNl();
-	lcdPrint("ed: "); lcdPrint(IntToStr((uintptr_t)&_edata,8,F_HEX));lcdNl();
-	lcdPrint("dr: "); lcdPrint(IntToStr((uintptr_t)&_data_rom,8,F_HEX));lcdNl();
-	lcdPrint("edr: "); lcdPrint(IntToStr((uintptr_t)&_edata_rom,8,F_HEX));lcdNl();
-	*/
-	lcdPrint("global: "); lcdPrint(IntToStr(global,8,F_HEX));lcdNl();
+	lcdPrint("PC:      "); lcdPrint(IntToStr(get_pc(),8,F_HEX));lcdNl();
+	lcdPrint("StackP:  "); lcdPrint(IntToStr(get_sp(),8,F_HEX));lcdNl();
+	lcdPrint("ShadowR: "); lcdPrint(IntToStr(CREG_M4MEMMAP,8,F_HEX));lcdNl();
+	lcdPrint("text_s:  "); lcdPrint(IntToStr((uintptr_t)&_text_start,8,F_HEX));lcdNl();
+	lcdPrint("text_e:  "); lcdPrint(IntToStr((uintptr_t)&_text_end,8,F_HEX));lcdNl();
+	lcdPrint("reloc_ep:"); lcdPrint(IntToStr((uintptr_t)&_reloc_ep,8,F_HEX));lcdNl();
+	lcdPrint("end:     "); lcdPrint(IntToStr((uintptr_t)&_end,8,F_HEX));lcdNl();
 	lcdDisplay();
 
 	uint8_t key;
@@ -156,35 +123,9 @@ int main(void) {
 	setSystemFont();
 
 	static const struct MENU main={ "main 1", {
-		{ "Copy", &doCopy},
+		{ "Info", &doInfo},
 		{NULL,NULL}
 	}};
 	handleMenu(&main);
 	return 0;
 }
-
-extern uint32_t _size;
-extern uint32_t * _reloc_ep;
-
-void __attribute__ ((naked)) reset_handler(void){
-	volatile unsigned *dest;
-	volatile uint32_t *src;
-	volatile uint32_t idx;
-
-	if ((void *)CREG_M4MEMMAP != &_reloc_ep){
-		src=(uint32_t *)CREG_M4MEMMAP;
-		for (idx=0; idx < ((uintptr_t)& _size)/sizeof(uint32_t); idx++){
-			((uint32_t*)&_reloc_ep)[idx]= src[idx];
-		};
-		CREG_M4MEMMAP = (uintptr_t)&_reloc_ep;
-		boot(&_reloc_ep);
-	}else{
-		for (dest = &_bss; dest < &_ebss; ) {
-			*dest++ = 0;
-		}
-	};
-
-	/* Call the application's entry point. */
-	main();
-}
-
