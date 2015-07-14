42fe1aaac3ff851cee861700279562a34a540a06
Stefan `Sec` Zehl <sec@42.org>
2015-07-14 22:17:08 +0200

    blink systick only inside speed test

--- testapp/speed.c
+++ testapp/speed.c
@@ -9,21 +9,26 @@
 void init_speed(){
 };
 
+static uint8_t tickon=0;
+
 void tick_speed(){
 	static int ctr=0;
-	if (++ctr%15==1)
-		TOGGLE(LED4);
+	if (tickon)
+		if (++ctr%15==1)
+			TOGGLE(LED4);
 };
 
 //# MENU speed
 void speed_menu(){
 	getInputWaitRelease();
+	SETUPgout(LED4);
+	tickon=1;
 	lcdClear();
 	lcdPrintln("Speed.");
 	lcdPrintln("up:   204");
 	lcdPrintln("down: 102");
 	lcdPrintln("left:  12");
-	lcdPrintln("right: 20");
+	lcdPrintln("right: 16");
 	lcdDisplay();
 	ON(LED1);
 
@@ -39,14 +44,16 @@ void speed_menu(){
 				cpu_clock_set(12);
 				break;
 			case BTN_RIGHT:
-				cpu_clock_set(20);
+				cpu_clock_set(16);
 				break;
 			case BTN_ENTER:
+				tickon=0;
+				OFF(LED4);
 				return;
 		};
-		lcdSetCrsr(0,5*8);
+		lcdSetCrsr(0,6*8);
 		lcdPrint("Speed:");
-		lcdPrint(IntToStr(_cpu_speed,8,0));
+		lcdPrint(IntToStr(_cpu_speed,4,F_LONG));
 		lcdNl();
 		lcdDisplay();
 	};
