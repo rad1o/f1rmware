1b4333298b00c640dc80d16b01f73f9004875248
Stefan `Sec` Zehl <sec@42.org>
2015-07-14 22:59:14 +0200

    MSC test app

---
+++ testapp/msc.c
@@ -0,0 +1,26 @@
+#include <r0ketlib/display.h>
+#include <r0ketlib/print.h>
+#include <r0ketlib/keyin.h>
+#include <r0ketlib/itoa.h>
+#include <rad1olib/pins.h>
+
+#include <rad1olib/setup.h>
+#include <lpcapi/msc/msc_main.h>
+#include <libopencmsis/core_cm3.h>
+
+//# MENU MSC
+void msc_menu(){
+	MSCenable();
+	lcdPrintln("MSC enabled.");
+	getInputWaitRelease();
+	while(getInputRaw()!=BTN_ENTER){
+		if(getInputRaw()==BTN_RIGHT)
+			lcdPrintln(".");
+		lcdDisplay();
+		__WFI();
+	};
+	lcdPrintln("disconnect");
+	lcdDisplay();
+	MSCdisable();
+	getInputWaitRelease();
+};
