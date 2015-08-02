7dd5ff14ecf2da007214ed4bca31e225efd82508
Stefan `Sec` Zehl <sec@42.org>
2015-08-02 17:47:52 +0200

    Implement l0dable support

---
+++ l0dables/blinky.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 team rad1o
+ *
+ * This is a small sample for a l0dable
+ *
+ */
+
+#include <libopencm3/lpc43xx/gpio.h>
+#include <rad1olib/pins.h>
+#include <rad1olib/setup.h>
+#include <r0ketlib/keyin.h>
+#include <r0ketlib/itoa.h>
+#include "usetable.h"
+
+void ram(void){
+	lcdPrintln("l0dable-Test");
+	lcdDisplay();
+
+	lcdNl();
+	lcdDisplay();
+	while(1){
+	    lcdSetCrsr(0,8*8);
+	    lcdPrint("ctr=");
+	    lcdPrint(IntToStr(_timectr/1000,5,F_LONG));
+	    lcdDisplay();
+	    switch(getInputRaw()){
+		case BTN_ENTER:
+		    return;
+		case BTN_LEFT:
+		    TOGGLE(LED3);
+		    break;
+		case BTN_RIGHT:
+		    TOGGLE(LED3);
+		    delay(10000000);
+		    break;
+		case BTN_UP:
+		    ON(LED3);
+		    break;
+		case BTN_DOWN:
+		    OFF(LED3);
+		    break;
+	    };
+	}
+	getInputWaitRelease();
+	return;
+}
