5ba61cc80f681ed59144fe4efddb8751f6181d19
iggy <iggy@yggi.de>
2015-08-09 01:00:26 +0200

    nick_plain with fg/bg color

--- l0dables/nick_plain.c
+++ l0dables/nick_plain.c
@@ -13,22 +13,27 @@
 #include "usetable.h"
 
 void ram(void) {
-    int dx=0;
-    int dy=0;
+  int dx=0;
+  int dy=0;
     static uint32_t ctr=0;
-    ctr++;
+  ctr++;
 
-    setExtFont(GLOBAL(nickfont));
-    dx=DoString(0,0,GLOBAL(nickname));
+  setExtFont(GLOBAL(nickfont));
+  dx=DoString(0,0,GLOBAL(nickname));
     dx=(RESX-dx)/2;
     if(dx<0)
-	dx=0;
+        dx=0;
     dy=(RESY-getFontHeight())/2;
 
-    lcdClear();
-    DoString(0,dy,GLOBAL(nickname));
-    lcdDisplay();
+  lcdClear();
+  lcdFill(GLOBAL(nickbg));
+  setTextColor(GLOBAL(nickbg),GLOBAL(nickfg));
+  lcdSetCrsr(dx,dy);
+  lcdPrint(GLOBAL(nickname));
+  lcdDisplay();
 
-    getInputWait();
-    return;
+  getInputWait();
+  setTextColor(0xFF,0x00);
+  return;
+  
 }
