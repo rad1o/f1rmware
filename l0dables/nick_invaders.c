2dcf8345e4cf8ed89aaba6b4e536bf47736c369c
iggy <iggy@yggi.de>
2015-08-09 01:19:30 +0200

    nick_invaders working

--- l0dables/nick_invaders.c
+++ l0dables/nick_invaders.c
@@ -1,5 +1,5 @@
 /* nick_invaders.c
- *  
+ *
  *  By NiciDieNase
  *  r0ket@nicidienase.de
  *
@@ -9,23 +9,28 @@
  *
  */
 
-#include <sysinit.h>
 #include <string.h>
-#include "basic/basic.h"
-#include "lcd/render.h"
-#include "lcd/display.h"
+
+#include <r0ketlib/config.h>
+#include <r0ketlib/display.h>
+#include <r0ketlib/fonts.h>
+#include <r0ketlib/render.h>
+#include <r0ketlib/print.h>
+#include <r0ketlib/keyin.h>
+
+#include "invfont.c"
+
 #include "usetable.h"
-#include "lcd/fonts.h"
-#include "lcd/fonts/invaders.h"
-#include "lcd/fonts/invaders.c"
+
+
 
 void ram(void)
 {
   bool step = true;
   const int minX = 10;
   const int minY = 10;
-  const int maxX = 50;
-  const int maxY = 30;
+  const int maxX = 70;
+  const int maxY = 60;
   const int space = 15;
   const int delaytime = 150;
   int x = minX;
@@ -39,11 +44,11 @@ void ram(void)
   dx=(RESX-dx)/2;
   if(dx<0)
       dx=0;
-  dy=40;
-  
+  dy=80;
+  setTextColor(GLOBAL(nickbg),GLOBAL(nickfg));
   while(1){
-    lcdFill(0);
-    font = &Font_Invaders;
+    lcdFill(GLOBAL(nickbg));
+    setIntFont(&Font_Invaders);
     // Draw UFO
     DoChar(-15+u,1,'U');
     u+=2;
@@ -61,23 +66,23 @@ void ram(void)
       // move down on right end
     case 1:
       y++;
-      dir++;      
+      dir++;
       if(y == maxY) y=minY;
       break;
       // move left
-    case 2:      
+    case 2:
       x--;
       if(x==minX) dir++;
-      break; 
+      break;
       // move down on left end
     case 3:
       y++;
-      dir++;      
+      dir++;
       if(y == maxY) y=minY;
       break;
     }
     dir %= 4;
-    font = NULL;
+    setExtFont(GLOBAL(nickfont));
     DoString(dx,dy,nickname);
     lcdDisplay();
     if(getInputRaw()!=BTN_NONE) return;
