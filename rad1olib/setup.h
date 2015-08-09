b58db0d7c57e0482ea244e07cfd4d01281e8ac87
Stefan `Sec` Zehl <sec@42.org>
2015-08-09 23:54:37 +0200

    Current state of the affair

--- rad1olib/setup.h
+++ rad1olib/setup.h
@@ -28,12 +28,14 @@
 #include <stdbool.h>
 
 extern uint8_t _cpu_speed;
-void delay(uint32_t duration);
+void delayNop(uint32_t duration);
 
-void cpu_clock_init(void);
+void cpuClockInit(void);
 void ssp_clock_init(void);
 void cpu_clock_set(uint32_t target_mhz);
 void usb_clock_init(void);
 void hackrf_clock_init(void);
+void si5351_init(void);
+
 
 #endif
--- testapp/ppack.c
+++ testapp/ppack.c
@@ -12,6 +12,7 @@
 #include <r0ketlib/keyin.h>
 #include <r0ketlib/menu.h>
 #include <r0ketlib/select.h>
+#include <r0ketlib/idle.h>
 #include <fatfs/ff.h>
 #include <r0ketlib/fs_util.h>
 
@@ -19,33 +20,69 @@
 #include <rad1olib/pins.h>
 
 #include <portapack_hackrf/portapack.h>
+#include <common/hackrf_core.h>
+#include <common/rf_path.h>
+#include <common/sgpio.h>
+#include <libopencm3/lpc43xx/dac.h>
 
-//# MENU aappack
+
+extern uint32_t sctr;
+extern uint16_t *sram;
+
+//# MENU Apack
 void ppack_menu() {
 	lcdClear();
 	lcdPrintln("PPack port");
-	lcdPrintln("up:   ");
-	lcdPrintln("down: ");
-	lcdPrintln("l/r:  ");
+	lcdPrintln("");
+	lcdPrintln("up=enable");
 	lcdDisplay();
+	dac_init(false);
+
+	cpu_clock_set(204); // WARP SPEED! :-)
+	hackrf_clock_init();
+	rf_path_pin_setup();
+	/* Configure external clock in */
+	scu_pinmux(SCU_PINMUX_GP_CLKIN, SCU_CLK_IN | SCU_CONF_FUNCTION1);
+
+	sgpio_configure_pin_functions();
+
+	ON(EN_VDD);
+	ON(EN_1V8);
+	OFF(MIC_AMP_DIS);
+	uint16_t * samples;
 
 	while(1){
-		switch(getInputWait()){
+		switch(getInputRaw()){
 			case BTN_UP:
-			    ON(EN_VDD);
 			    // ON(MIXER_EN); // hackrf does this
-			    OFF(MIC_AMP_DIS);
 			    cpu_clock_set(204); // WARP SPEED! :-)
+			    si5351_init();
+			    portapack_init();
+			    getInputWaitRelease();
 
 			    break;
 			case BTN_DOWN:
-				break;
+			    lcdPrintln("audio");
+			    for(samples=(uint16_t*)0x20000000;samples<sram;samples++){
+				dac_set(samples[0]);
+				delayNop(765);
+			    };
+
+			    break;
 			case BTN_LEFT:
-				break;
+			    lcdPrintln("reset");
+			    sram=(uint16_t*)0x20000000;
+			    break;
 			case BTN_RIGHT:
 				break;
 			case BTN_ENTER:
 				return;
 		};
+		TOGGLE(LED2);
+		delayms(40);
+		lcdPrint(IntToStr((uintptr_t)sram,8,F_HEX));
+		lcdPrint(" ");
+		lcdPrintln(IntToStr(sctr,7,F_LONG));
+		lcdDisplay();
 	};
 };
