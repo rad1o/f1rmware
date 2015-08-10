4e6d7accadc606e54357836024276312badd8d1b
Stefan `Sec` Zehl <sec@42.org>
2015-08-10 22:57:51 +0200

    very simple image and anim nick l0dables

---
+++ l0dables/nick_anim.c
@@ -0,0 +1,13 @@
+#include <r0ketlib/config.h>
+#include <r0ketlib/display.h>
+#include <r0ketlib/fonts.h>
+#include <r0ketlib/render.h>
+#include <r0ketlib/print.h>
+#include <r0ketlib/keyin.h>
+#include <r0ketlib/image.h>
+
+#include "usetable.h"
+
+void ram(void){
+    lcdShowAnim("nick.an1");
+}
