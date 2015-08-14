ae9b730b72a496303f09549953c362afbe18af8c
klonfish <git+rad1o@klonfish.de>
2015-08-14 02:52:46 +0200

    Fixed size of buffer for led animation

--- campapp/rgb_leds.c
+++ campapp/rgb_leds.c
@@ -2,16 +2,17 @@
 #include <r0ketlib/fs_util.h>
 
 #define MAX_LED_FRAMES 50
+#define BUF_SIZE 3*8*MAX_LED_FRAMES+2
 
-unsigned char leds[3*8*MAX_LED_FRAMES];
+unsigned char leds[BUF_SIZE];
 unsigned int frames = 0;
 
 void rgbLedsInit(void) {
 	char filename[] = "rgb_leds.hex";
 	int size = getFileSize(filename);
 	if(size > 0) {
-		if(size >= 3*8*MAX_LED_FRAMES)
-			size = 3*8*MAX_LED_FRAMES;
+		if(size >= BUF_SIZE)
+			size = BUF_SIZE;
 		readFile(filename, (char*)leds, size);
 		frames = (size-1)/(3*8);
 	}
