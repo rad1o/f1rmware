6b8420d48c9c620cd5273d9b06821ab6a2f05dd7
Benjamin Neff <benjamin@coding4coffee.ch>
2015-08-15 18:01:15 +0200

    add draw functions to rad1olib

---
+++ rad1olib/draw.h
@@ -0,0 +1,9 @@
+#ifndef F1RMWARE_DRAW_H
+#define F1RMWARE_DRAW_H
+
+void drawHLine(int y, int x1, int x2, uint8_t color);
+void drawVLine(int x, int y1, int y2, uint8_t color);
+
+void drawRectFill(int x, int y, int width, int heigth, uint8_t color);
+
+#endif //F1RMWARE_DRAW_H
