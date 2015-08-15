7c818108d855cd51385545b2d4068ae99cd65109
s01jacccc <s01jacccc@users.noreply.github.com>
2015-08-16 01:02:08 +0200

    correct bash algo to create maze - TODO: move to better language for simple description

--- ccccmaze/story/gen/maze.sh
+++ ccccmaze/story/gen/maze.sh
@@ -9,25 +9,49 @@ rm -rf ${namespace}
 
 mkdir -p ${namespace}
 for ((i=0;i<size;i++)); do
-	#statements
-	echo $pi $pj
+	# x
+	pi=$(printf "%02X" $i)
 	for ((j=0;j<size;j++)); do
-		pi=$(printf "%02X" $i)
+		# y
 		pj=$(printf "%02X" $j)
 		fn=${namespace}/${namespace}$pi$pj.cfg
+
 		touch ${fn}
-	    up=$(printf "%s%02X" ${pi} $((j-1)))
-		down=$(printf "%s%02X" ${pi} $((j+1)))
-		left=$(printf "%02X%s" $((i-1)) ${pj})
-		right=$(printf "%02X%s" $((i+1)) ${pj})
+		nexti=$(printf "%02X" $((i+1)))
+		nextj=$(printf "%02X" $((j+1)))
+
+		previ=$(printf "%02X" $((i-1)))
+		prevj=$(printf "%02X" $((j-1)))
+		
+	    up=$(printf "%s%s" ${pi} ${prevj})
+		down=$(printf "%s" ${pi} ${nextj})
+		left=$(printf "%s" ${nexti} ${pj})
+		right=$(printf "%s" ${nexti} ${pj})
+
+		if [ $j == $((size - 1)) ]; then
+			down=----
+		fi
+		if [ $i == $((size - 1))  ]; then
+			up=----
+		fi
+
+		if [ $prevj == "FFFFFFFFFFFFFFFF" ]; then
+			up=----
+		fi
+		if [ $previ == "FFFFFFFFFFFFFFFF" ]; then
+			left=----
+		fi
+		echo $up $down $left $right
+		if [ $right == "FFFFFFFFFFFFFFFF" ]; then
+			right=----
+		fi
+
 		walls=$(echo "${up}${down}${left}${right}")
-		walls=$(echo $walls | sed 's|FFFFFFFFFFFFFFFF|--|g;')
 		echo ${walls} >> ${fn}
 		for ((z=0;z<15;z++)); do
   		    line=$(mktemp -u XXXXXXXXXXXXXXXXXXXX)
   		    echo "${line}" >> ${fn}
   		done
-  		echo ${fn}
-  		ls ${namespace}
+  		#echo ${fn}
 	done
 done
