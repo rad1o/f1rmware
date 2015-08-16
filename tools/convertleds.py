c3857020954a24e05ae0562d5fbccea43c06e056
Stefan `Sec` Zehl <sec@42.org>
2015-08-16 09:52:40 +0200

    Fix convertleds and convert all anims for smartflash

--- tools/convertleds.py
+++ tools/convertleds.py
@@ -2,13 +2,34 @@
 import sys
 import binascii
 import struct
+import getopt
+import re
 
-try:
-    basename = sys.argv[1]
-    filename = 'assets/' + basename + '.led'
-    delay = int(sys.argv[2])
-except:
-    sys.stderr.write('Usage: convertleds.py <name> <delay>\n')
+options, remainder = getopt.getopt(sys.argv[1:], 'd:o:h', ['delay=', 'outdir=', 'help'
+                                                    ])
+delay = 50
+outdir = "."
+
+for opt, arg in options:
+    if opt in ('-d', '--delay'):
+        delay = int(arg)
+    if opt in ('-o', '--outdir'):
+        outdir = arg
+    if opt in ('-h', '--help'):
+        sys.stderr.write('Usage:\n')
+        sys.stderr.write('\tconvertleds.py [-d delay] input.led [output.l3d]\n')
+
+filename = remainder[0]
+
+if len(remainder)==1:
+    outfilename = re.sub('\.[^.]*$','.l3d',filename)
+    outfilename = outdir + "/" + re.sub('.*/','',outfilename)
+    
+else:
+    outfilename = remainder[1]
+
+if len(remainder)>2:
+    sys.stderr.write('too many arguments\n')
     sys.exit(1)
 
 output = ''
@@ -17,7 +38,6 @@ with open(filename) as fp:
     contents = fp.read()
     contents = contents.replace(chr(10), '')
     contents = contents.replace(' ', '')
-    newname = 'files/' + basename + '.l3d'
-    with open(newname, 'w') as fpW:
+    with open(outfilename, 'w') as fpW:
         fpW.write(struct.pack('>H', delay))
         fpW.write(binascii.unhexlify(contents))
